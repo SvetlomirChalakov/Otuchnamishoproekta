73fc9234cc0971dcfb119c7e7cd7eb3148e4b91b
SvetlomirChalakov <finndips279@gmail.com>
2024-02-21 09:41:33 +0200

    MishosProjectotChas

--- ListEven/Program.cs
+++ ListEven/Program.cs
@@ -1,19 +1,41 @@
-﻿namespace ListEven
+﻿using System.Collections.Generic;
+
+namespace ListEven
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             List<int> list = Console.ReadLine().Split().Select(int.Parse).ToList();
+            //otpechatvane na funkciite
+           
+            Console.WriteLine("Nechetni"+ string.Join("",GetEvenElements(list)));
+            Console.WriteLine("Chetni"+ string.Join("",GetOddElements(list)));
+        }
+        static List<int> GetEvenElements(List<int> inputs)
+        {
             List<int> listEven = new List<int>();
-            for (int i = 0; i < list.Count; i++)
+            for (int i = 0; i < inputs.Count; i++)
             {
-                if (list[i]%2 == 0)
+                if (inputs[i] % 2 == 0)
                 {
-                    listEven.Add(list[i]);
+                    listEven.Add(inputs[i]);
                 }
             }
-            Console.WriteLine(string.Join(" ",listEven));
+            return listEven;
+        }
+
+        static List<int> GetOddElements(List<int> inputs)
+        {
+            List<int> listodd = new List<int>();
+            for (int i = 0; i < inputs.Count; i++)
+            {
+                if (inputs[i] % 2 != 0)
+                {
+                    listodd.Add(inputs[i]);
+                }
+            }
+            return listodd;
         }
     }
 }
